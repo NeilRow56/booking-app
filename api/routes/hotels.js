cc0d2c440ff225e2efc7f062941fccf8d08e70ac
Neil <neilrowland56@yahoo.com>
2022-05-27 06:58:11 +0100

    All controllers and routes  added for create, update, delete,get and getAll

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,15 +1,24 @@
 import express from 'express';
 //We are importing the Hotel mode we have just created
 import Hotel from '../models/Hotel.js';
-import { createHotel } from '../controllers/hotel.js';
+import {
+	createHotel,
+	updateHotel,
+	deleteHotel,
+	getHotel,
+	getHotels,
+} from '../controllers/hotel.js';
 
 const router = express.Router();
 
 //CREATE
 router.post('/', createHotel);
 //UPDATE
+router.put('/:id', updateHotel);
 //DELETE
+router.delete('/:id', deleteHotel);
 //GET
+router.get('/:id', getHotel);
 //GET ALL
-
+router.get('/', getHotels);
 export default router;
