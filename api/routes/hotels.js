7ff07b9aae4e6d0ddbb54260308a9f34a5814288
Neil <neilrowland56@yahoo.com>
2022-05-28 07:58:42 +0100

    Added user model, authentication and verification

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,6 +1,5 @@
 import express from 'express';
-//We are importing the Hotel mode we have just created
-import Hotel from '../models/Hotel.js';
+
 import {
 	createHotel,
 	updateHotel,
@@ -8,15 +7,17 @@ import {
 	getHotel,
 	getHotels,
 } from '../controllers/hotel.js';
+import { verifyAdmin } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
 //CREATE
-router.post('/', createHotel);
+router.post('/', verifyAdmin, createHotel);
+
 //UPDATE
-router.put('/:id', updateHotel);
+router.put('/:id', verifyAdmin, updateHotel);
 //DELETE
-router.delete('/:id', deleteHotel);
+router.delete('/:id', verifyAdmin, deleteHotel);
 //GET
 router.get('/:id', getHotel);
 //GET ALL
